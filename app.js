a0572c33cdfc32c7984491155dad06ef0477656b
Argério Queiroz da Silva Filho <argerioaf@gmail.com>
2021-06-14 09:27:34 -0300

    https adicionado ao servidor

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const path = require('path');
 const accountsLinkController = require('./src/controllers/accountsLink/accountsLinkController')
 const app = express();
 const env = process.env.NODE_ENV || 'development';
-const envDir = path.join(__dirname,`./src/configs/env/${env}`)
+const envDir = path.join(__dirname,`./src/configs/env/${env}`);
+const https = require('https');
+const fs = require('fs');
 
 require(envDir)(app);
 // require('./src/configs/passport')(app);
 require('./src/index')(app);
 
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 
-app.listen(app.get('port'), async () => {
+
+https.createServer(options,app).listen(app.get('port'), async () => {
   console.log('Aplicação executando na porta ', app.get('port'));
   accountsLinkController.changeStatusPubsub(false,'');
 });
