31b542a4e4622958b873b756bfb4194404bf46b4
Argério Queiroz da Silva Filho <argerioaf@gmail.com>
2021-06-14 10:28:19 -0300

    correcao

--- app.js
+++ app.js
@@ -3,21 +3,14 @@ const path = require('path');
 const accountsLinkController = require('./src/controllers/accountsLink/accountsLinkController')
 const app = express();
 const env = process.env.NODE_ENV || 'development';
-const envDir = path.join(__dirname,`./src/configs/env/${env}`);
-const https = require('https');
-const fs = require('fs');
+const envDir = path.join(__dirname,`./src/configs/env/${env}`)
 
 require(envDir)(app);
 // require('./src/configs/passport')(app);
 require('./src/index')(app);
 
-const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
 
-
-https.createServer(options,app).listen(app.get('port'), async () => {
+app.listen(app.get('port'), async () => {
   console.log('Aplicação executando na porta ', app.get('port'));
   accountsLinkController.changeStatusPubsub(false,'');
 });
