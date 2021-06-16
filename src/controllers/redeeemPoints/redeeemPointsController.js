7fc9e64fdad9218b5d622e274a9171a523420077
Argério Queiroz da Silva Filho <argerioaf@gmail.com>
2021-06-16 13:26:12 -0300

    adicionado regra para listar apenas os resgates de pontos referente ao canal que solicitou

--- src/controllers/redeeemPoints/redeeemPointsController.js
+++ src/controllers/redeeemPoints/redeeemPointsController.js
@@ -1,6 +1,7 @@
 
 const RedeemPoints = require('../../schemas/RedeemPoints');
 const dotenv = require('dotenv');
+const Channel = require('../../schemas/channel');
 dotenv.config();
 // const botController = require('../../controllers/bot/botController');
 
@@ -9,45 +10,58 @@ const listRedeemPoints = async (req, res) => {
     console.log('req.userId: ',req.userId);
     const id_user = req.userId;
     try {
-        let redeeems_quant = 0;
-        let redeeems = [];
-        let find = status?{status:status}:{}
-        if (last) {
-            redeeems = await RedeemPoints.find(find)
-            .populate('id_user')
-            .populate('id_channel')
-            .limit(limit * 1)
-            .skip((page - 1) * limit)
-            .sort('-_id')
-            .exec();
-            redeeems_quant = await RedeemPoints.find(find)
-            .sort('-_id')
-            .exec();
-            redeeems_quant = redeeems_quant.length;
-        }else{
-            redeeems = await RedeemPoints.find(find)
-            .populate('id_user')
-            .populate('id_channel')
-            .limit(limit * 1)
-            .skip((page - 1) * limit)
-            .exec();
-            redeeems_quant = await RedeemPoints.find(find)
-            .sort('-_id')
-            .exec();
-            redeeems_quant = redeeems_quant.length;
+        let channel = await Channel.findOne({id_person:id_user});
+        if (channel) {
+            let redeeems_quant = 0;
+            let redeeems = [];
+            let find = {id_channel:channel._id};
+            if (status) {
+                find ={
+                    ...find,
+                    status:status
+                };
+            }
+            if (last) {
+                redeeems = await RedeemPoints.find(find)
+                .populate('id_user')
+                .populate('id_channel')
+                .limit(limit * 1)
+                .skip((page - 1) * limit)
+                .sort('-_id')
+                .exec();
+                redeeems_quant = await RedeemPoints.find(find)
+                .sort('-_id')
+                .exec();
+                redeeems_quant = redeeems_quant.length;
+            }else{
+                redeeems = await RedeemPoints.find(find)
+                .populate('id_user')
+                .populate('id_channel')
+                .limit(limit * 1)
+                .skip((page - 1) * limit)
+                .exec();
+                redeeems_quant = await RedeemPoints.find(find)
+                .sort('-_id')
+                .exec();
+                redeeems_quant = redeeems_quant.length;
+            }
+            
+            const count = redeeems_quant;
+            let totalPages = Math.ceil(count / limit);
+            res.status(200).json({
+              data:redeeems,
+              totalPages: totalPages,
+              currentPage: page,
+              total_itens: redeeems_quant
+            });
+        } else {
+            res.status(400).send({
+                message:'Erro ao listar resgates de pontos, canal não vinculado ao usuario'
+            });
         }
-        
-        const count = redeeems_quant;
-        let totalPages = Math.ceil(count / limit);
-        res.status(200).json({
-          data:redeeems,
-          totalPages: totalPages,
-          currentPage: page,
-          total_itens: redeeems_quant
-        });
     } catch (error) {
           res.status(400).send({
-              message:'ERRO ao listar resgates de pontos',
+              message:'Erro ao listar resgates de pontos',
               error:error
           });
     }
